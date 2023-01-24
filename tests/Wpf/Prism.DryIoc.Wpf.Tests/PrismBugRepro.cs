f49786824cf9158e69cd88f9a8fba9bf07a781c8
Alexander Zabluda <azabluda@gmail.com>
2023-01-24 16:52:10 +0100

    Add failing unit test TransientDelegateRegistration

---
+++ tests/Wpf/Prism.DryIoc.Wpf.Tests/PrismBugRepro.cs
@@ -0,0 +1,26 @@
+﻿using DryIoc;
+using Prism.Ioc;
+using Xunit;
+
+namespace Prism.DryIoc.Wpf.Tests
+{
+    public class PrismBugRepro
+    {
+        [StaFact]
+        public void TransientDelegateRegistration()
+        {
+            // Arrange
+            global::DryIoc.Container container = new(Rules.Default.WithDefaultReuse(Reuse.Singleton));
+            DryIocContainerExtension containerExtension = new(container);
+            int counter = 0;
+            containerExtension.Register<int>(_ => ++counter);
+
+            // Act
+            int a = containerExtension.Resolve<int>();
+            int b = containerExtension.Resolve<int>();
+
+            // Assert
+            Assert.Equal(a + 1, b);
+        }
+    }
+}
